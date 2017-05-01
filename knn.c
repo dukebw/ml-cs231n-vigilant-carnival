e218b218937a3b2b4b80d7092f81567bdbd482de
Brendan Duke <dukebw@mcmaster.ca>
2017-04-30 22:44:20 -0400

    Fixed seg fault in reading data_batch_1.bin.

--- knn.c
+++ knn.c
@@ -11,16 +11,14 @@ int main(void)
 {
         int32_t status;
         FILE *data_handle = fopen("cifar-10-batches-bin/data_batch_1.bin",
-                                  "r");
+                                  "rb");
 
         for (uint32_t label_index = 0;
              label_index < NUM_LABELS_PER_BATCH;
              ++label_index) {
                 printf("label %d: %d\n", label_index, fgetc(data_handle));
 
-                status = fseek(data_handle,
-                               BYTES_PER_IMAGE,
-                               ftell(data_handle));
+                status = fseek(data_handle, BYTES_PER_IMAGE, SEEK_CUR);
                 assert(status == 0);
         }
 
